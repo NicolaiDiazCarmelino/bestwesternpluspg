3fc3ac04eb519acb5006b50863b0bfbf3e9f222c
NicolaiDiazCarmelino <diazcarmelino@gmail.com>
2025-01-28 17:43:44 -0800

    carga documento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { UploadCityLedgerComponent } from './upload-city-ledger/upload-city-ledger.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path:'upload-c-ledger', component: UploadCityLedgerComponent }
+];
---
+++ src/app/upload-city-ledger/upload-city-ledger.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-city-ledger',
+  imports: [],
+  templateUrl: './upload-city-ledger.component.html',
+  styleUrl: './upload-city-ledger.component.scss'
+})
+export class UploadCityLedgerComponent {
+
+}
